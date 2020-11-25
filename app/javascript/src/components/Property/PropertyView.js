b04567262018e75fa1757bf7e3c073d2b19eb818
Ben <benbaumann95@hotmail.co.uk>
2020-11-25 13:08:41 +0000

    Add error message when API fails to fetch data

--- app/javascript/src/components/Property/PropertyView.js
+++ app/javascript/src/components/Property/PropertyView.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { getProperty } from '../../utils/api/properties'
 import PropertyDetails from './PropertyDetails'
 import Spinner from '../Spinner/Spinner'
+import ErrorMessage from '../Errors/ErrorMessage/ErrorMessage'
 
 const PropertyView = (props) => {
   const [property, setProperty] = useState({})
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
 
   const getPropertyView = async (propertyReference) => {
+    setError(null)
+
     try {
       const data = await getProperty(propertyReference)
       setProperty(data)
     } catch (e) {
       setProperty(null)
-      console.log('An error has occured:', e)
+      console.log('An error has occured:', e.response)
+      setError(`Oops an error occurred with error status: ${e.response?.status}`)
     }
 
     setLoading(false)
@@ -39,6 +44,7 @@ const PropertyView = (props) => {
               hierarchyType={property.hierarchyType}
             />
           }
+          {error && <ErrorMessage label={error} />}
         </>
       )}
     </>
