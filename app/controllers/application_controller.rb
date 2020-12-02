3e1273c435d47d2269837008be30b9fd3b35176c
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-12-02 09:39:25 +0000

    Block IP addresses
    This is a temporary measure and can be removed once we add user
    authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :block_unlisted_ip_addresses
+
+private
+
+  def block_unlisted_ip_addresses
+    # This temporary IP blocking can be removed
+    # after authentication is added
+
+    if (ENV["RESTRICT_IP_ADDRESSES"] == "yes") &&
+      allowed_ip_addresses.exclude?(request.remote_ip)
+
+      head :forbidden
+    end
+  end
+
+  def allowed_ip_addresses
+    ENV.fetch("ALLOWED_IP_ADDRESSES", "").split(",")
+  end
 end
